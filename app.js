f201ec69cedcf98ad0d0b9fdc390f3a8ea7dedf8
Mr Oluwole <juwon.o@outlook.com>
2024-08-30 15:37:15 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+    <title>Basic Embed</title>
+    <title>Add Title Here</title>
+
+    <script type="text/javascript"
+	    src="https://public.tableau.com/views/Covid19dashboard_17249520769460/Dashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"></script>
+@@ -11,7 +11,7 @@
+
+<body>
+
+  <h1>Embedding Tableau</h1>
+  <h1>Add Title Here</h1>
+
+    <button id = "exportPDF">Generate PDF</button>
+    <button id = "exportImage">Generate Image</button>
